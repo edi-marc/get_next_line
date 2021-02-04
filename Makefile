3f58118f23a35dba135914bf513d6f2c9110ce99
Emanuele Di marco <edi-marc@e3r1p3.42roma.it>
2021-02-04 19:34:01 +0100

    main changed, added some test files, Makefile added

---
+++ Makefile
@@ -0,0 +1,60 @@
+# **************************************************************************** #
+#                                                                              #
+#                                                         :::      ::::::::    #
+#    Makefile                                           :+:      :+:    :+:    #
+#                                                     +:+ +:+         +:+      #
+#    By: edi-marc <marvin@42.fr>                    +#+  +:+       +#+         #
+#                                                 +#+#+#+#+#+   +#+            #
+#    Created: 2021/02/04 17:24:47 by edi-marc          #+#    #+#              #
+#    Updated: 2021/02/04 19:27:56 by edi-marc         ###   ########.fr        #
+#                                                                              #
+# **************************************************************************** #
+
+NAME = gnl
+
+BNAME = gnl_b
+
+SRC = get_next_line.c get_next_line.h get_next_line_utils.c
+
+BSRC = get_next_line_bonus.c get_next_line_bonus.h get_next_line_utils_bonus.c
+
+MAIN = main_get_next_line.c
+
+GCH = get_next_line.h.gch get_next_line_bonus.h.gch
+
+CC = gcc
+
+FLAGS = -Wall -Werror -Wextra
+
+MFLAGS = -D
+
+BUF = BUFFER_SIZE=
+
+bf =
+
+RENAME = mv
+
+STD_NAME = a.out
+
+RM = /bin/rm -rf
+
+all: $(NAME)
+
+bonus: $(BNAME)
+
+clean: 
+	@$(RM) $(GCH) $(NAME) $(BNAME) $(STD_NAME)
+
+re: clean all
+
+rbonus: clean bonus
+
+$(NAME) : $(SRC) $(MAIN)
+	@$(CC) $(FLAGS) $(MFLAGS) $(BUF)$(bf) $(SRC) $(MAIN)
+	@$(RENAME) $(STD_NAME) $(NAME)
+	
+$(BNAME) : $(BSRC) $(MAIN)
+	@$(CC) $(FLAGS) $(MFLAGS) $(BUF)$(bf) $(BSRC) $(MAIN)
+	@$(RENAME) $(STD_NAME) $(BNAME)
+
+.PHONY: all bonus clean re rbonus
--- main_get_next_line.c
+++ main_get_next_line.c
@@ -6,11 +6,11 @@
 /*   By: edi-marc <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/02 13:43:04 by edi-marc          #+#    #+#             */
-/*   Updated: 2021/02/02 17:40:55 by edi-marc         ###   ########.fr       */
+/*   Updated: 2021/02/04 18:30:10 by edi-marc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-/**
+/*
 **
 **	Main used to check the correct output of get_next_line function
 **	
@@ -18,18 +18,21 @@
 **
 **	argv[1] = "C"					TEST read & write function
 **	argv[1] = "0"					READ FROM stdin
-**	argv[1] = "name_of_the_file"	READ FROM text file (in ~/Desktop/)
+**	argv[1] = "name_of_the_file"	READ FROM text file (in current directory)
+**	
+**	argv[1] = "name_of_the_file"	READ FROM text file (in current directory)
+**	argv[2] = "E"					till the EOF
 **
 */
 
 #include "get_next_line.h"
 
 #include <stdio.h>
-//#include <fcntl1.h>
+#include <fcntl.h>
 
 int main (int argc , char **argv)
 {
-	if(argc == 2)
+	if (argc == 2)
 	{
 		/* pag. 171 - The C Programming Language */
 		/* simple program to copy its input to its output */
@@ -40,25 +43,80 @@ int main (int argc , char **argv)
 
 			while ((n = read(0, buf, BUFFER_SIZE)) > 0)
 				write(1, buf, n);
+
+			return (0);
 		}
 		if (*argv[1] == '0')
 		{
 			char *p_line;
 			char **line;
+			int check;
 
 			p_line = NULL;
 			line = &p_line;
 			printf("\n--- PRIMA ---\n");
-			printf("line : %s\n", *line);
+			printf("%s\n", *line);
 			printf("\n--- DOPO ---\n");
-			if (get_next_line(0, line))
-				printf("line : %s\n", *line);
+			if ((check = get_next_line(0, line)))
+				printf("%s\n\n", *line);
+			else if (check == -1)
+				printf("An error happened\n");
+			else if (check == 0)
+				printf("EOF has been reached\n");
 
+			return (0);
 		}
 		if (*argv[1])
 		{
+			int fd;
 
+			if ((fd = open(argv[1], O_RDONLY)) == -1)
+				printf("can't open %s\n", argv[1]);
+			else
+			{	
+				char *p_line;
+				char **line;
+				int check;
+
+				p_line = NULL;
+				line = &p_line;
+				printf("\n--- PRIMA ---\n");
+				printf("%s\n", *line);
+				printf("\n--- DOPO ---\n");
+				if ((check = get_next_line(fd, line)))
+					printf("%s\n\n", *line);
+				else if (check == -1)
+					printf("An error happened\n");
+				else if (check == 0)
+					printf("EOF has been reached\n");
+			}
+
+			return (0);
 		}
 	}
+	if (argc == 3)
+	{
+		if (*argv[1] && *argv[2] == 'E')
+		{	
+			int fd;
+
+			if ((fd = open(argv[1], O_RDONLY)) == -1)
+				printf("can't open %s\n", argv[1]);
+			else
+			{	
+				char *p_line;
+				char **line;
+
+				p_line = NULL;
+				line = &p_line;
+				printf("\n--- FILE ---\n");
+				while ((get_next_line(fd, line)) > 0)
+					printf("%s\n", *line);
+			}
+
+			return (0);
+		}
+	}
+
 	return (0);
 }
