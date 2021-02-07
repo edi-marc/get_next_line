bb3cd358aa60b4c390ead31d4b50df983ae8b445
Emanuele Di marco <edi-marc@e3r2p6.42roma.it>
2021-02-07 17:14:37 +0100

    test functions modified

--- test_get_next_line.c
+++ test_get_next_line.c
@@ -6,9 +6,15 @@
 /*   By: edi-marc <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/06 18:34:10 by edi-marc          #+#    #+#             */
-/*   Updated: 2021/02/06 19:15:37 by edi-marc         ###   ########.fr       */
+/*   Updated: 2021/02/07 17:14:15 by edi-marc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+/*
+**
+**	Only for testing purpose during development
+**
+*/
 
 #include "get_next_line.h"
 
