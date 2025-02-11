947a872b5b919cb53e08718f2647e4358db1e4e9
emdi-mar <>
2025-02-11 21:30:31 +0100

    utils started

--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -6,8 +6,35 @@
 /*   By: emdi-mar <emdi-mar@student.42roma.it>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/11 19:59:46 by emdi-mar          #+#    #+#             */
-/*   Updated: 2025/02/11 19:59:49 by emdi-mar         ###   ########.fr       */
+/*   Updated: 2025/02/11 21:29:13 by emdi-mar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "get_next_line.h"
 
+static char	*buffer_heap(int fd, char *str)
+{
+	
+}
+
+char	*next_line(char *str)
+{
+	size_t	i;
+
+	i = 0;
+	if (!str)
+		return (NULL);
+	while (str[i] && str[i] != '\n')
+		i++;
+	new = (char *)malloc(sizeof(char) * (i + 2));
+	if (!new)
+		return (NULL);
+	i = 0;
+	while (str[i] && str[i] != '\n')
+	{
+		new[i] = str[i];
+		i++;
+	}
+	new[i] = '\0';
+	return (new);
+}
