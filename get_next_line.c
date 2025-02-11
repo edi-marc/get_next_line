b277fbb6b4e892ea78157d20d3d5c6f907c5229b
emdi-mar <>
2025-02-11 20:48:48 +0100

    gnl Header and function

--- get_next_line.c
+++ get_next_line.c
@@ -6,8 +6,35 @@
 /*   By: emdi-mar <emdi-mar@student.42roma.it>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/11 19:59:25 by emdi-mar          #+#    #+#             */
-/*   Updated: 2025/02/11 19:59:30 by emdi-mar         ###   ########.fr       */
+/*   Updated: 2025/02/11 20:47:23 by emdi-mar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "get_next_line.h"
 
+size_t	ft_strlen(const char *s)
+{
+	size_t	i;
+
+	i = 0;
+	if (!s)
+		return (0);
+	while (s[i])
+		i++;
+	return (i);
+}
+
+char	*get_next_line(int fd)
+{
+	char		*line;
+	static char	*buffer;
+
+	if (BUFFER_SIZE <= 0 || fd < 0)
+		return (NULL);
+	buffer = buffer_heap(fd, line);
+	if (!buffer)
+		return (NULL);
+	line = next_line(buffer);
+	clean_line(buffer);
+	return (line);
+}
--- get_next_line.h
+++ get_next_line.h
@@ -6,8 +6,21 @@
 /*   By: emdi-mar <emdi-mar@student.42roma.it>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/11 19:59:38 by emdi-mar          #+#    #+#             */
-/*   Updated: 2025/02/11 19:59:41 by emdi-mar         ###   ########.fr       */
+/*   Updated: 2025/02/11 20:45:54 by emdi-mar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
 
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 42
+# endif
+
+char		*get_next_line(int fd);
+size_t		ft_strlen(const char *s);
+static char	*buffer_heap(int fd, char *str);
+char		*next_line(static char *str);
+void		clean_line(static char *str);
+
+#endif
